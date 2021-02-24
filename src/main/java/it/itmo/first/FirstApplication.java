e211231f4b7fa5d09eeab4be7e59fd63c97fcf8e
Dima Ludov <lDummyl>
2021-02-24 18:28:42 +0300

    bean creation simple examples

--- src/main/java/it/itmo/first/FirstApplication.java
+++ src/main/java/it/itmo/first/FirstApplication.java
@@ -1,13 +1,21 @@
 package it.itmo.first;
 
+import it.itmo.first.beans.SomeBean;
+import it.itmo.first.conf.MyConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class FirstApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FirstApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(FirstApplication.class, args);
+
+		SomeBean myConfig =  context.getBean("someBean", SomeBean.class);
+		System.out.println("myConfig = " + myConfig);
+
+
 	}
 
 }
